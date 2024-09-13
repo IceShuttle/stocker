1902f72216c6f9bc15f4187ba018091f968bda38
IceShuttle <IceShuttle@Debian>
2024-09-14 03:39:15 +0530

    all 3 apis working

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 It is the backend for fetching stock data made with Rust(Axum)
-It fetches data when you pass symbol as query for example "localhost:3000/fetch?symbol=TCS"
-It implements read through cache strategy(i.e. it initially tries to read cache but if it misses than it fetches from the server and save it in cache)
-As data is volatile I have used redis as it is simple,high performance and transient
+It fetches data when you pass symbol as query for example "localhost:3000/fetchcurrent?symbol=TCS"
+It implements read through cache strategy(i.e. it initially tries to read cache but if it misses than it fetches from the server and save it in cache) also it follow an expiration time of 5 seconds for realtime data and 1 minute for day's data
+As data is volatile I have used redis as it is simple and high performance
 to Run it you need to
 1. Enter "docker-compose up -d"
+
+It has 3 APIs(working)
+1. "localhost:3000/fetchcurrent/(stocksymbol)" fetches current price
+2. "localhost:3000/fetchday/(stocksymbol)" fetcher whole days price data
+3. "localhost:3000/fetch/all" fetches all stored data
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,11 @@
-use ::time::macros::format_description;
-
+use futures::stream::TryStreamExt;
 use serde::{Deserialize, Serialize};
-use time::Duration;
 use yahoo_finance_api::{self as yahoo};
 
 use axum::{extract::Path, http::StatusCode, routing::get, Json, Router};
 use fred::prelude::*;
-use tokio::{sync::OnceCell, task, time};
+use fred::types::Scanner;
+use tokio::sync::OnceCell;
 
 static RC: OnceCell<RedisClient> = OnceCell::const_new();
 
@@ -23,6 +22,10 @@ struct StockData {
 struct DayStockData {
     data: Vec<StockData>,
 }
+#[derive(Serialize, Deserialize)]
+struct AllStockData {
+    data: Vec<DayStockData>,
+}
 
 #[tokio::main]
 async fn main() {
@@ -33,8 +36,8 @@ async fn main() {
     client.init().await.unwrap();
     RC.set(client).unwrap();
     let app = Router::new()
-        .route("/", get(root))
-        .route("/fetch/:symbol", get(fetch_price))
+        .route("/fetchcurrent/:symbol", get(fetch_price))
+        .route("/fetch/all", get(fetch_all))
         .route("/fetchday/:symbol", get(fetch_price_day));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -68,7 +71,7 @@ async fn fetch_price(Path(symbol): Path<String>) -> Result<Json<StockData>, Stat
             };
             let qstr = serde_json::to_string(&sd).unwrap();
             let _: () = rc
-                .set(final_symbol, &qstr, Some(Expiration::EX(60)), None, false)
+                .set(final_symbol, &qstr, Some(Expiration::EX(5)), None, false)
                 .await
                 .unwrap();
 
@@ -77,7 +80,7 @@ async fn fetch_price(Path(symbol): Path<String>) -> Result<Json<StockData>, Stat
     }
 }
 
-async fn fetch_price_day(Path(symbol): Path<String>) -> Result<Json<DayStockData>, StatusCode> {
+async fn get_day_data(symbol: String) -> Result<DayStockData, StatusCode> {
     let rc = RC.get().unwrap();
     let final_symbol = format!("{}.NS", symbol);
     let key = format!("{}_day", final_symbol);
@@ -85,7 +88,7 @@ async fn fetch_price_day(Path(symbol): Path<String>) -> Result<Json<DayStockData
     match quote_str {
         Some(val) => {
             let out: DayStockData = serde_json::from_str(&val).unwrap();
-            Ok(Json(out))
+            Ok(out)
         }
         None => {
             let provider = yahoo::YahooConnector::new().unwrap();
@@ -107,80 +110,100 @@ async fn fetch_price_day(Path(symbol): Path<String>) -> Result<Json<DayStockData
                 qs.data.push(sd);
             }
             let qstr = serde_json::to_string(&qs).unwrap();
-            let _: () = rc.set(&key, &qstr, None, None, false).await.unwrap();
+            let _: () = rc
+                .set(&key, &qstr, Some(Expiration::EX(60)), None, false)
+                .await
+                .unwrap();
 
-            Ok(Json(qs))
+            Ok(qs)
         }
     }
 }
 
-async fn root() -> &'static str {
-    "Hello, World!"
+async fn fetch_price_day(Path(symbol): Path<String>) -> Result<Json<DayStockData>, StatusCode> {
+    Ok(Json(get_day_data(symbol).await.unwrap()))
 }
-async fn fetching_data() {
-    let nifty50 = [
-        "ADANIPORTS",
-        "ASIANPAINT",
-        "AXISBANK",
-        "BAJAJ-AUTO",
-        "BAJAJFINSV",
-        "BAJFINANCE",
-        "BHARTIARTL",
-        "BPCL",
-        "BRITANNIA",
-        "CIPLA",
-        "COALINDIA",
-        "DIVISLAB",
-        "DRREDDY",
-        "EICHERMOT",
-        "GRASIM",
-        "HCLTECH",
-        "HDFC",
-        "HDFCBANK",
-        "HDFCLIFE",
-        "HEROMOTOCO",
-        "HINDALCO",
-        "HINDUNILVR",
-        "ICICIBANK",
-        "INDUSINDBK",
-        "INFY",
-        "ITC",
-        "JSWSTEEL",
-        "KOTAKBANK",
-        "LT",
-        "M&M",
-        "MARUTI",
-        "NESTLEIND",
-        "NTPC",
-        "ONGC",
-        "POWERGRID",
-        "RELIANCE",
-        "SBILIFE",
-        "SBIN",
-        "SUNPHARMA",
-        "TATACONSUM",
-        "TATAMOTORS",
-        "TATASTEEL",
-        "TCS",
-        "TECHM",
-        "TITAN",
-        "ULTRACEMCO",
-        "UPL",
-        "WIPRO",
-    ];
-    let nifty50_symbol: Vec<String> = nifty50.into_iter().map(|x| format!("{}.NS", x)).collect();
-
-    loop {
-        time::sleep(Duration::from_secs(5)).await;
-        println!("Fetching Data");
-        match RC.get() {
-            Some(rc) => {
-                let _: () = rc.set("hallo", "abcd", None, None, false).await.unwrap();
-                println!("Values updated");
-            }
-            None => {
-                eprintln!("Cannot connect to redis")
+async fn fetch_all() -> Result<Json<AllStockData>, StatusCode> {
+    let rc = RC.get().unwrap();
+    let mut out: Vec<DayStockData> = Vec::new();
+    let mut scan_stream = rc.scan("*_day", Some(10), None);
+    while let Some(mut page) = scan_stream.try_next().await.unwrap() {
+        if let Some(keys) = page.take_results() {
+            for k in keys {
+                let sd: String = rc.get(&k).await.unwrap();
+                out.push(serde_json::from_str(&sd).unwrap());
             }
         }
     }
+
+    let fout = AllStockData { data: out };
+    Ok(Json(fout))
 }
+
+// async fn fetching_data() {
+//     let nifty50 = [
+//         "ADANIPORTS",
+//         "ASIANPAINT",
+//         "AXISBANK",
+//         "BAJAJ-AUTO",
+//         "BAJAJFINSV",
+//         "BAJFINANCE",
+//         "BHARTIARTL",
+//         "BPCL",
+//         "BRITANNIA",
+//         "CIPLA",
+//         "COALINDIA",
+//         "DIVISLAB",
+//         "DRREDDY",
+//         "EICHERMOT",
+//         "GRASIM",
+//         "HCLTECH",
+//         "HDFC",
+//         "HDFCBANK",
+//         "HDFCLIFE",
+//         "HEROMOTOCO",
+//         "HINDALCO",
+//         "HINDUNILVR",
+//         "ICICIBANK",
+//         "INDUSINDBK",
+//         "INFY",
+//         "ITC",
+//         "JSWSTEEL",
+//         "KOTAKBANK",
+//         "LT",
+//         "M&M",
+//         "MARUTI",
+//         "NESTLEIND",
+//         "NTPC",
+//         "ONGC",
+//         "POWERGRID",
+//         "RELIANCE",
+//         "SBILIFE",
+//         "SBIN",
+//         "SUNPHARMA",
+//         "TATACONSUM",
+//         "TATAMOTORS",
+//         "TATASTEEL",
+//         "TCS",
+//         "TECHM",
+//         "TITAN",
+//         "ULTRACEMCO",
+//         "UPL",
+//         "WIPRO",
+//     ];
+//     let nifty50_symbol: Vec<String> = nifty50.into_iter().map(|x| format!("{}.NS", x)).collect();
+//
+//     loop {
+//         time::sleep(Duration::from_secs(5)).await;
+//         println!("Fetching Data");
+//         match RC.get() {
+//             Some(rc) => {
+//                 let _: () = rc.set("hallo", "abcd", None, None, false).await.unwrap();
+//                 println!("Values updated");
+//             }
+//             None => {
+//                 eprintln!("Cannot connect to redis")
+//             }
+//         }
+//     }
+// }
